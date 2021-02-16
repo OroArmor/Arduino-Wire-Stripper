972fdc891df58f1ef9c265e780814d6cbaecc988
OroArmor <eliorona@live.com>
2021-02-15 19:46:27 -0800

    Mostly finished, just need actual values

--- Wire_Stripper.ino
+++ Wire_Stripper.ino
@@ -45,44 +45,181 @@ Button right(RIGHT_BUTTON, rightButton, rightButtonHold);
 int servo_position = 0;
 Servo servo;
 
+int wireLength = 0;
+int wireCount = 0;
+
+enum MenuState {
+  START,
+  LENGTH,
+  COUNT,
+  CUTTING
+};
+
+MenuState menuState = START;
+
+
+enum CutState {
+  NONE = 0,
+  STRIP_BEGINNING = 1,
+  MIDDLE = 2,
+  STRIP_END = 3
+};
+
+int cutState = NONE;
+
+
+int index = 0;
+long startTime;
+
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
-
   servo.attach(8);
+  servo.write(SERVO_MIN);
 
   stepper.setSpeed(STEPPER_SPEED);
+
+  startTime = millis();
 }
 
+boolean notInc = true;
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(servo_position);
-  lcd.print("   ");
-  stepper.step(2);
-  servo.write(servo_position);
+  lcd.setCursor(0, 0);
+  if (menuState == CUTTING) {
+    lcd.clear();
+    cutState++;
+  }
+
+  switch (menuState) {
+    case START:
+      lcd.print("Wire Cutter");
+      lcd.setCursor(0, 1);
+      lcd.print(String("Press the middle button to start").substring(index, index + 16));
+      break;
+    case LENGTH:
+      lcd.print("Wire length: ");
+      lcd.setCursor(0, 1);
+      lcd.print(wireLength);
+      break;
+    case CUTTING:
+      lcd.print("Wires to cut:");
+      lcd.setCursor(13, 0);
+      lcd.print((cutState == STRIP_BEGINNING ? "beg" : (cutState == MIDDLE ? "mid" : "end")));
+      goto PRINT_WIRE_COUNT;
+    case COUNT:
+      lcd.print("Wire count: ");
+PRINT_WIRE_COUNT:
+      lcd.setCursor(0, 1);
+      lcd.print(wireCount + 1);
+      break;
+  }
+
+  if (cutState == STRIP_BEGINNING) {
+    stepper.step(500);
+    writeThenReturnServo(SERVO_MID);
+  } else if (cutState == MIDDLE) {
+    stepper.step(1000);
+    writeThenReturnServo(SERVO_MID);
+  } else if (cutState == STRIP_END) {
+    stepper.step(500);
+    writeThenReturnServo(SERVO_MAX);
+
+    wireCount--;
+    cutState = NONE;
+  }
+
+  if (wireCount == -1) {
+    menuState = START;
+    wireCount = 0;
+    lcd.clear();
+  }
+
+  long deltaTime = millis() - startTime;
+  if (deltaTime < 10000) {
+    if (deltaTime < 9000 && deltaTime > 1000) {
+      if (deltaTime % 500 < 50) {
+        if (notInc) {
+          index += 1;
+          notInc = false;
+        }
+      } else {
+        notInc = true;
+      }
+    }
+  } else {
+    index = 0;
+    startTime = millis();
+  }
+
   left.tick();
   middle.tick();
   right.tick();
 }
 
+void writeThenReturnServo(int setPosition) {
+  servo.write(setPosition);
+  delay(500);
+  servo.write(SERVO_MIN);
+  delay(500);
+}
+
 void leftButton(bool pushed) {
   if (pushed) {
-      servo_position = SERVO_MIN;
+    lcd.clear();
+    switch (menuState) {
+      case START:
+      case CUTTING:
+        break;
+      case LENGTH:
+        wireLength--;
+        break;
+      case COUNT:
+        wireCount--;
+        break;
+    }
+    if (wireLength < 0) {
+      wireLength = 0;
+    }
+
+    if (wireCount < 0) {
+      wireCount = 0;
+    }
   }
 }
 void middleButton(bool pushed) {
   if (pushed) {
-    servo_position = SERVO_MID;
+    lcd.clear();
+    switch (menuState) {
+      case START:
+        menuState = LENGTH;
+      case CUTTING:
+        break;
+      case LENGTH:
+        menuState = COUNT;
+        break;
+      case COUNT:
+        menuState = CUTTING;
+        break;
+    }
   }
 }
 void rightButton(bool pushed) {
   if (pushed) {
-      servo_position = SERVO_MAX;
+    lcd.clear();
+    switch (menuState) {
+      case START:
+      case CUTTING:
+        break;
+      case LENGTH:
+        wireLength++;
+        break;
+      case COUNT:
+        wireCount++;
+        break;
+    }
   }
 }
 
@@ -90,7 +227,6 @@ void leftButtonHold() {
   leftButton(true);
 }
 void middleButtonHold() {
-  middleButton(true);
 }
 void rightButtonHold() {
   rightButton(true);
